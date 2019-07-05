751537da298464c1e854ecff4b063b5a3d67581f
ritthidet <51473714+ritthidet@users.noreply.github.com>
2019-07-05 14:02:12 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,28 +1,12 @@
 <?php
-
-
-
 require "vendor/autoload.php";
-
-$access_token = '02Mdx0LuTY/sosRaTc1eGY5wczzpBsQU4aPxpN96qZIEA4Okq9sfhMQzbsibnBgHlqeoqS1FE+NvJRlX/19TYplIQAokNPj2CNKkBQaeJl876GabgAw+Pe1kFbXHcVBrT8zzPmkO6/HNnk88YGO2ZQdB04t89/1O/w1cDnyilFU=
-';
-
+$access_token = '02Mdx0LuTY/sosRaTc1eGY5wczzpBsQU4aPxpN96qZIEA4Okq9sfhMQzbsibnBgHlqeoqS1FE+NvJRlX/19TYplIQAokNPj2CNKkBQaeJl876GabgAw+Pe1kFbXHcVBrT8zzPmkO6/HNnk88YGO2ZQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '59b18a0f68945f67ebc14f73940e5eb1
 ';
-
-$pushID = 'U9616e8240298e2efa9de3b5cf2056e5e';
-
+$idPush = 'U9616e8240298e2efa9de3b5cf2056e5e'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-
-
-
-
-
-
