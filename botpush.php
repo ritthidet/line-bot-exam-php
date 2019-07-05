406a51b377f2ae6cfc5e711b4abf8f2b24aecd1b
ritthidet <51473714+ritthidet@users.noreply.github.com>
2019-07-05 13:52:40 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '02Mdx0LuTY/sosRaTc1eGY5wczzpBsQU4aPxpN96qZIEA4Okq9sfhMQzbsibnBgHlqeoqS1FE+NvJRlX/
-19TYplIQAokNPj2CNKkBQaeJl876GabgAw+Pe1kFbXHcVBrT8zzPmkO6/HNnk88YGO2ZQdB04t89/1O/w1cDnyilFU=
+$access_token = '02Mdx0LuTY/sosRaTc1eGY5wczzpBsQU4aPxpN96qZIEA4Okq9sfhMQzbsibnBgHlqeoqS1FE+NvJRlX/19TYplIQAokNPj2CNKkBQaeJl876GabgAw+Pe1kFbXHcVBrT8zzPmkO6/HNnk88YGO2ZQdB04t89/1O/w1cDnyilFU=
 ';
 
-$channelSecret = '59b18a0f68945f67ebc14f73940e5eb1';
+$channelSecret = '59b18a0f68945f67ebc14f73940e5eb1
+';
 
 $pushID = 'U9616e8240298e2efa9de3b5cf2056e5e';
 
